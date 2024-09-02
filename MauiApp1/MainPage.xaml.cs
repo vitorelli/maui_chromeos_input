dc910ed991ef2fc51f8d263fb58f1e3162d9daf2
Alexandre Vitorelli <alexandrevitorelli@hotmail.com>
2024-09-02 11:26:06 -0300

    Input issue on ChromeOS

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            if (sender is VisualElement editor) 
+            {
+                editor.Focus();
+            }
+        }
+    }
+
+}
